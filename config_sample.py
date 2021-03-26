e32cc3cf916009bbdf181eec50aaefbcda4f2d7e
Al Wirtes <wirtes@gmail.com>
2021-03-26 02:42:28 +0000

    created sample config

---
+++ config_sample.py
@@ -0,0 +1,11 @@
+# Use these configurations to post to your Twitter Account
+
+#api key below
+CONSUMER_KEY = 'INSERT CONSUMER_KEY HERE'
+CONSUMER_SECRET = 'INSERT CONSUMER_SECRET HERE'
+#Access Token below
+ACCESS_KEY = 'INSERT ACCESS_KEY HERE'
+ACCESS_SECRET = 'INSERT ACCESS_SECRET HERE'
+
+#Rename this file as 'config.py' and insert your Twitter account information.
+
