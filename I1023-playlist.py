559c85f55ab22a6a09cbc150e6160c105e6f70b1
Al Wirtes <wirtes@gmail.com>
2021-03-25 21:09:47 -0600

    Comments are our friends

--- I1023-playlist.py
+++ I1023-playlist.py
@@ -12,6 +12,7 @@ api = Twython(config.CONSUMER_KEY, config.CONSUMER_SECRET, config.ACCESS_KEY, co
 lastPlayed = ""
 tweeted = ""
 
+# Call the playlist API, parse the currently playing song, form the tweet text.
 def getPlaylist():
     URL = 'http://playlist.cprnetwork.org/api/playlistCO?n='+str(int(time.time()))
     r = requests.get(URL)
@@ -22,15 +23,18 @@ def getPlaylist():
     tweetu = cap(tweetu, 277)
     return tweetu
 
+# Post the tweet
 def tweetLastPlayed(tweetText):
     curTime = str(time.strftime("%H:%M:%S"))
     print(curTime + " Tweeting: " + tweetText)
     api.update_status(status=tweetText)
     return
 
+# Function to trim the tweet at a specified length & add elipsis
 def cap(s, l):
     return s if len(s)<=l else s[0:l-3]+'...'
 
+# Store the last tweet in a local file
 def writeFile(log):
    lastTweetFile = open("/home/pi/python_scripts/I1023-playlist-v2/last_tweet.txt", "w")
    lastTweetFile.write(log)
@@ -38,6 +42,7 @@ def writeFile(log):
    lastTweetFile.close() 
    return
 
+# Read the last tweet from the local file
 def readFile():
     lastTweetFile = open("/home/pi/python_scripts/I1023-playlist-v2/last_tweet.txt", "r")
     lines = lastTweetFile.read()
@@ -50,14 +55,17 @@ def addDateSpecific(tText):
         tText = tText + " & GWAR"
     return tText
 
+# Get current song playing & compare to last tweet stored in the file.
+# If different, tweet the current song & store the tweet in the local file.
 def heavyLifting():
     tweeted = readFile()
     lastPlayed = getPlaylist()
     if tweeted != lastPlayed:
-        tweetLastPlayed(lastPlayed)  
-        writeFile(lastPlayed)        
+        tweetLastPlayed(lastPlayed)
+        writeFile(lastPlayed)
 
-
+# This is a hack, but it gets around the 1-minute granularity of cron
+# by running the call four times and sleeping 15 seconds between each call
 heavyLifting()
 time.sleep(15)
 heavyLifting()
