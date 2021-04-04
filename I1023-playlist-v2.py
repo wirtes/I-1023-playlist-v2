7e2ad045e77a216f1f655c56a5f0cf0a2949bab9
Al Wirtes <wirtes@gmail.com>
2021-04-04 10:29:57 -0600

    GWAR! GWAR! GWAR!

--- I1023-playlist-v2.py
+++ I1023-playlist-v2.py
@@ -4,14 +4,23 @@ import tweepy
 from datetime import datetime
 import time
 import sys
-# Get Settings from local config file
+
+# Get Settings from local config file & setup globals
 import config
-# Local Configurations
-last_tweet_file_path = "/home/pi/python_scripts/I1023-playlist-v2/last_tweet.txt"
+
+# Determine if it's April Fool's Day & set the EASTER_EGG value
+EASTER_EGG = False
+now = datetime.now()
+if now.strftime("%m-%d") == "04-01":
+    EASTER_EGG = True
+    # Only load the image libraries if we're going to use them
+    from PIL import Image
+
 
 def post_tweet_and_art(tweet, cover_image):
     """Post a tweet with art to Twitter"""
-    # Based on the excellent series of articles here: https://www.mattcrampton.com/blog/step_by_step_tutorial_to_post_to_twitter_using_python/
+    # Based on the excellent series of articles here: 
+    #   https://www.mattcrampton.com/blog/step_by_step_tutorial_to_post_to_twitter_using_python/
     twitter_auth_keys = { 
         "consumer_key"        : config.CONSUMER_KEY,
         "consumer_secret"     : config.CONSUMER_SECRET,
@@ -63,7 +72,7 @@ def cap(s, l):
 
 # Store the last tweet in a local file
 def write_file(log):
-    last_tweet_file = open(last_tweet_file_path, "w")
+    last_tweet_file = open(config.LAST_TWEET_FILE_PATH, "w")
     last_tweet_file.write(log)
     last_tweet_file.flush()
     last_tweet_file.close() 
@@ -71,24 +80,19 @@ def write_file(log):
 
 # Read the last tweet from the local file
 def read_file():
-    last_tweet_file = open(last_tweet_file_path, "r")
+    last_tweet_file = open(config.LAST_TWEET_FILE_PATH, "r")
     lines = last_tweet_file.read()
     last_tweet_file.close()
     return lines
-
-# Is it April Fool's Day? Add a joke. Ha ha.
-def easter_egg(tText):
-    now = datetime.now()
-    if now.strftime("%m-%d") == "04-01":
-        tText = tText + " & GWAR"
-    return tText
 
 def get_current_song():
     # I'm hard coding this location because everything about this function is specific to this one radio station.
     URL = 'https://playlist.cprnetwork.org/won_plus3/KVOQ.json'
     r = requests.get(URL)
     now_playing = r.json()
-    artist = easter_egg(now_playing[0]["artist"])
+    artist = now_playing[0]["artist"]
+    if EASTER_EGG:
+        artist += " & GWAR"
     title = now_playing[0]["title"]
     album = now_playing[0]["album"] 
     # Putting in a little hack because Indie 102.3 people only list the "album" as singles as "(Single)" sometimes.
@@ -99,8 +103,9 @@ def get_current_song():
     time = start_time.strftime("%-I:%M%P")
     # Compose the tweet text
     tweet_text = time + " " + title + " by " + artist + " from " + album
+    # Check if the tweet will be too long, trim it if it is.
     tweet_text = cap(tweet_text, 277)
-    # Use returned value for artist in case we changed the artist name on April Fools Day
+    # Use returned value for artist in case we changed the artist name on April Fool's Day
     album_art_lookup_text = album + " " + now_playing[0]["artist"]
     
     return {"tweet_text": tweet_text, "album_art_lookup_text": album_art_lookup_text, "title": title}
@@ -117,6 +122,13 @@ def main():
         write_file(current_song["tweet_text"])    
         if download_art(current_song["album_art_lookup_text"]):
             album_art_filename = config.ALBUM_ART_TEMP_FILE
+            # GWAR GWAR GWAR!
+            if EASTER_EGG:
+                # Open album art & overlay files, convert colorspace to match (RGBA), composite layers, output as new file.
+                background = Image.open(config.ALBUM_ART_TEMP_FILE).convert('RGBA')
+                foreground = Image.open(config.ALBUM_ART_OVERLAY_FILE).convert('RGBA')
+                Image.alpha_composite(background, foreground).save(config.ALBUM_ART_TEMP_FILE_EASTER_EGG)
+                album_art_filename = config.ALBUM_ART_TEMP_FILE_EASTER_EGG
         else:
             album_art_filename = "no_art"
         
--- config_sample.py
+++ config_sample.py
@@ -14,7 +14,9 @@ ACCESS_SECRET = 'INSERT ACCESS_SECRET HERE'
 
 # Temp file for album art
 ALBUM_ART_TEMP_FILE = 'INSERT PATH TO TEMP FILE FOR ALBUM ART HERE'
-
+# We only use the overlay for April Fool's day
+ALBUM_ART_OVERLAY_FILE = 'INSERT PATH TO OVERLAY FILE FOR ALBUM ART HERE'
+ALBUM_ART_TEMP_FILE_EASTER_EGG = 'INSERT PATH TO TEMP FILE FOR EASTER EGGED ALBUM ART HERE'
 
 #Rename this file as 'config.py' and insert your Twitter account information.
 
