c8fecd659d56d03514bbd773833d24b7044b5e0c
Al Wirtes <wirtes@gmail.com>
2022-07-23 17:06:32 +0100

    general cleanup

--- config_sample.py
+++ config_sample.py
@@ -13,14 +13,15 @@ ACCESS_SECRET = 'INSERT ACCESS_SECRET HERE'
 # ------------------------
 
 # Temp file for album art
-ALBUM_ART_TEMP_FILE = 'INSERT PATH TO TEMP FILE FOR ALBUM ART HERE'
+ALBUM_ART_TEMP_FILE = '/full/path/to/script/album_art/album_art_temp_file.png'
+
 # We store the last tweet in a file to maintain state. Only tweet when the file changes.
-LAST_TWEET_FILE_PATH = '/full/path/to/last/tweet/file.txt'
+LAST_TWEET_FILE_PATH = '/full/path/to/script/last_tweet/last_tweet.txt'
+
 # We only use the overlay for April Fool's day
-ALBUM_ART_OVERLAY_FILE = 'INSERT PATH TO OVERLAY FILE FOR ALBUM ART HERE'
-ALBUM_ART_TEMP_FILE_EASTER_EGG = 'INSERT PATH TO TEMP FILE FOR EASTER EGGED ALBUM ART HERE'
+ALBUM_ART_OVERLAY_FILE = '/full/path/to/script/GWAR-me/gwar_glow_overlay_with_text.png'
+ALBUM_ART_TEMP_FILE_EASTER_EGG = 'full/path/to/script/GWAR-me/gwar-ified.png'
 
 
-
-#Rename this file as 'config.py' and insert your Twitter account information.
-
+# Rename this file as 'config.py' and insert your Twitter account & local directory information.
+# Yeah, this could be a lot cleaner.
